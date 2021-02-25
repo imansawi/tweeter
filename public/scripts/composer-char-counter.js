033632a7ca59840f90f07883c6c9c7f2d7404ce6
imansawi <imansawi@hotmail.com>
2021-02-25 00:47:01 +0000

    Created navigation bar, header and a new-tweet container, using CSS for styling. created a function characters counter counts tweet characters using jQuery.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+const maxVal = 140;
+function characterCounter() {
+	const counter = $("#tweet-text").val().length;
+  
+	$('.counter').text(maxVal - counter);
+	$('.counter').text() < 0 ? $('.counter').addClass('invalid') : 
+	$('.counter').removeClass('invalid');
+	$('.errorMessage').slideUp('slow');
+}
+
+$(document).ready(function() {
+  $("#tweet-text").on('input', characterCounter);
+});
