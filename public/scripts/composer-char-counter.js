ec4dab507683fe5efd3a0ffb133347515d8b9db5
imansawi <imansawi@hotmail.com>
2021-02-26 01:13:46 +0000

    Created tweet container, hard coded in HTML, and its style

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,14 @@
 const maxVal = 140;
 function characterCounter() {
-	const counter = $("#tweet-text").val().length;
-  
-	$('.counter').text(maxVal - counter);
-	$('.counter').text() < 0 ? $('.counter').addClass('invalid') : 
-	$('.counter').removeClass('invalid');
-	$('.errorMessage').slideUp('slow');
+  const counter = $("#tweet-text").val().length;
+
+  $(".counter").text(maxVal - counter);
+  $(".counter").text() < 0
+    ? $(".counter").addClass("invalid")
+    : $(".counter").removeClass("invalid");
+  $(".errorMessage").slideUp("slow");
 }
 
-$(document).ready(function() {
-  $("#tweet-text").on('input', characterCounter);
+$(document).ready(function () {
+  $("#tweet-text").on("input", characterCounter);
 });
